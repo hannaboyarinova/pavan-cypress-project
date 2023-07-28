bfb159d26d66b417280c167a5033b22467ad4cea
Hanna Boyarinova <hanna.boyarinova@itechart-group.com>
2023-07-28 17:47:15 +0300

    initialized test project

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'o36jxu',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
