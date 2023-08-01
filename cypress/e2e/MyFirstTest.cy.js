5fccf7b51d28c5286e8d7c971a62c3c21dd892c1
Hanna Boyarinova <hanna.boyarinova@itechart-group.com>
2023-08-01 14:17:36 +0300

    added seconf test

--- cypress/e2e/MyFirstTest.cy.js
+++ cypress/e2e/MyFirstTest.cy.js
@@ -1,6 +1,10 @@
 describe('My first test suite', () => {
-	it('test1', () => {
+	it('verify title - positive', () => {
 		cy.visit('https://opensource-demo.orangehrmlive.com/')
 		cy.title().should('eq', 'OrangeHRM')
+	})
+	it('verifify title - negative', () => {
+		cy.visit('https://opensource-demo.orangehrmlive.com/')
+		cy.title().should('not.eq', 'OrangeHRM123')
 	})
 })
