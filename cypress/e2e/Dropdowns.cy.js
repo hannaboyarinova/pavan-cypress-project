282f3d117a856d4cfe22d4bf55fcb2c50f28678e
Hanna Boyarinova <hanna.boyarinova@itechart-group.com>
2023-09-22 17:34:57 +0300

    add testcase for autosuggest dropdown

--- cypress/e2e/Dropdowns.cy.js
+++ cypress/e2e/Dropdowns.cy.js
@@ -1,5 +1,8 @@
 describe('Handle dropdowns', () => {
 	const dropdownUrl = '/dropdowns'
+	const dummyUrl =
+		'https://www.dummyticket.com/dummy-ticket-for-visa-application/'
+	const wikiUrl = 'https://www.wikipedia.org/'
 	it('Dropdown with select', () => {
 		cy.visit(dropdownUrl)
 		cy.get('#dropdown-menu')
@@ -8,10 +11,17 @@ describe('Handle dropdowns', () => {
 			.should('have.value', 'Belarus')
 	})
 
-	it.only('Dropdown without select', () => {
-		cy.visit('https://www.dummyticket.com/dummy-ticket-for-visa-application/')
+	it('Dropdown without select', () => {
+		cy.visit(dummyUrl)
 		cy.get('#select2-billing_country-container').click()
 		cy.get('.select2-search__field').type('Belarus', '{ enter }')
 		cy.get('#select2-billing_country-container').should('have.text', 'Belarus')
+	})
+
+	it.only('Auto suggest dropdown', () => {
+		cy.visit(wikiUrl)
+		cy.get('#searchInput').type('Belarus')
+		cy.get('.suggestion-title').contains('Belarusian language').click()
+		cy.get('#firstHeading').should('contain', 'Belarusian language')
 	})
 })
