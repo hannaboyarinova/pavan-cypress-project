9b4aca8c45c5859f60eb5f120ffc51edf26841e5
Hanna Boyarinova <hanna.boyarinova@itechart-group.com>
2023-09-29 16:54:43 +0300

    add 8 lecture and definitions

--- cypress/e2e/Dropdowns.cy.js
+++ cypress/e2e/Dropdowns.cy.js
@@ -2,7 +2,9 @@ describe('Handle dropdowns', () => {
 	const dropdownUrl = '/dropdowns'
 	const dummyUrl =
 		'https://www.dummyticket.com/dummy-ticket-for-visa-application/'
+	const googleUrl = 'https://www.google.com/'
 	const wikiUrl = 'https://www.wikipedia.org/'
+
 	it('Dropdown with select', () => {
 		cy.visit(dropdownUrl)
 		cy.get('#dropdown-menu')
@@ -18,10 +20,23 @@ describe('Handle dropdowns', () => {
 		cy.get('#select2-billing_country-container').should('have.text', 'Belarus')
 	})
 
-	it.only('Auto suggest dropdown', () => {
+	it('Auto suggest dropdown', () => {
 		cy.visit(wikiUrl)
 		cy.get('#searchInput').type('Belarus')
 		cy.get('.suggestion-title').contains('Belarusian language').click()
 		cy.get('#firstHeading').should('contain', 'Belarusian language')
+	})
+
+	it.only('Dynamic dropdown', () => {
+		cy.visit(googleUrl)
+		cy.get('[name = "q"]').type('cypress automation')
+		cy.wait(3000)
+		cy.get('div.wM6W7d>span').should('have.length', 12)
+		cy.get('div.wM6W7d>span').each(($el, index, $list) => {
+			if ($el.text() == 'cypress automation resume') {
+				cy.wrap($el).click()
+			}
+		})
+		cy.get('[name = "q"]').should('have.value', 'cypress automation resume')
 	})
 })
